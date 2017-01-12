c025604f8ddfa4f3724b161c5dfb1c202d83b618
alexsyrom <alexsyrom@yandex.ru>
2017-01-12 12:51:39 +0300

    games

---
+++ 01.12/202/prog.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+
+template <class Class>
+void Print(Class instance) {
+    std::cout << instance << std::endl;
+}
+
+void Print() {
+    std::cout << std::endl;
+}
+
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    // std::ifstream input("input.txt");
+    // std::ofstream output("output.txt");
+    size_t number;
+    std::cin >> number;
+    
+    return 0;
+}
+
+
